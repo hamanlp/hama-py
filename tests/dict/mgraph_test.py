5f16939167dcac35f6a6c9ca4deea74ef3fd769d
Seongmin Park <llov0708@gmail.com>
2020-03-24 21:35:40 -0400

    Add tests for exceptions

--- tests/dict/mgraph_test.py
+++ tests/dict/mgraph_test.py
@@ -4,7 +4,20 @@ from hama.dict import MGraph
 
 def test_query():
     graph = MGraph()
+
+    with pytest.raises(Exception):
+        graph.query('')
+
     graph.load()
     assert (graph.query('') == False)
     assert (graph.query('nbjc') == True)
     assert (graph.query('xxx') == False)
+
+def test_wrong_attr():
+    graph = MGraph()
+    assert(graph.booboo == None)
+
+def test_unload():
+    graph = MGraph()
+    graph.unload()
+    assert(graph.dict == None)
