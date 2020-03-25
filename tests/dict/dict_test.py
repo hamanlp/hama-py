068feb31e0687b159c6c41d1e9c4de7b511a01c0
Seongmin Park <llov0708@gmail.com>
2020-03-24 21:35:40 -0400

    Add tests for exceptions

--- tests/dict/dict_test.py
+++ tests/dict/dict_test.py
@@ -1,10 +1,22 @@
 import pytest
 from hama.dict import Dict
 
-
 def test_query():
     dict = Dict()
+
+    with pytest.raises(Exception):
+        dict.query('')
+
     dict.load()
     assert (dict.query('') == [])
     assert (set(dict.query('아버지')) == set(['nc']))
     assert (set(dict.query('가')) == set(['ep', 'xp', 'nc', 'jc', 'ec']))
+
+def test_wrong_attr():
+    dict = Dict()
+    assert(dict.booboo == None)
+
+def test_unload():
+    dict = Dict()
+    dict.unload()
+    assert(dict.dict == None)
