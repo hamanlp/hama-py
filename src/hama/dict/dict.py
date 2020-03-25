12de369d33deb03d1343cb1a32aca140815a93e8
Seongmin Park <llov0708@gmail.com>
2020-03-24 20:09:12 -0400

    Switch to bloom filters

--- src/hama/dict/dict.py
+++ src/hama/dict/dict.py
@@ -1,12 +1,15 @@
+import os
+import configparser
 from .singleton import Singleton
-import pandas as pd
+from .bloomfilter import LookupBloomFilter
 
 
 class Dict(metaclass=Singleton):
     """Class responsible for managing embedded morpheme dictionary.
 
     Attributes:
-        dict (obj): Morpheme dictionary.
+        dict (dic): Morpheme dictionaries.
+        Keys include KAIST POS22 tags.
     """
     
     def __init__(self):
@@ -15,11 +18,35 @@ class Dict(metaclass=Singleton):
         super().__init__()
         self.dict = None 
 
+    def __getattr__(self, name):
+        """Called when an called attribute does not exist."""
+        return None
+
     def load(self):
         """Loads morpheme dictionary into memory."""
         if self.dict is None:
-            self.dict = pd.read_csv('morphemes.csv')
 
+            config_path = os.path.join(
+                    os.path.dirname(__file__), 'meta/source.ini')
+            config = configparser.ConfigParser()
+            config.read(config_path)
+
+            fp = config['FILTER_PATH']
+            hc = config['HASH_COUNT']
+            sz = config['FILTER_SIZE']
+
+            self.dict = {}
+            for name, path in fp.items():
+                if name[0] == 'd':
+                    filter = LookupBloomFilter(
+                            path=path,
+                            size=int(sz[name]),
+                            hash_count=int(hc[name]))
+                    filter.load()
+
+                    tag = name[2:]
+                    self.dict[tag] = filter
+        
     def unload(self):
         """Unloads morpheme dictionary from memory."""
         self.dict = None
@@ -32,14 +59,19 @@ class Dict(metaclass=Singleton):
     
         Returns:
             list: list containing tags of morpheme.
+
+        Raises:
+            Exception if dictionary was not initialized
+            before with load().
         """
+
+        if self.dict is None:
+            raise Exception("Initialize dict before querying!")
+
         tags = []
-        # Change when using bloom filters.
-        entries = self.dict.loc[self.dict.term.isin([m])]
-        if len(entries) > 0:
-            for row in entries.itertuples():
-                tags.append(row.pos22)
-        ########################################
+        for tag, dict in self.dict.items():
+            if dict.query(m):
+                tags.append(tag)
         return tags 
 
 
