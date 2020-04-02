86aaf3e553426810608701d224d46d85ee135765
Seongmin Park <llov0708@gmail.com>
2020-04-01 20:14:35 -0400

    Add hmm tests

--- tests/hmm/hmm_test.py
+++ tests/hmm/hmm_test.py
@@ -1,12 +1,27 @@
 import pytest
 from hama.hmm import TagHMM
 
+
+def test_query():
+    hmm = TagHMM()
+
+    hmm.unload()
+    with pytest.raises(Exception):
+        hmm.query('s', 's')
+
+    hmm.load()
+    with pytest.raises(Exception):
+        hmm.query('gibberish', 'tag')
+    hmm.query('s', 's')
+
+
 def test_wrong_attr():
     hmm = TagHMM()
-    asset(hmm.foofoo == None)
+    assert (hmm.foofoo == None)
+
 
 def test_unload():
     hmm = TagHMM()
     hmm.unload()
-    assert(hmm.hmm == None)
-
+    assert (hmm.hmm == None)
+    assert (hmm.t2i == None)
