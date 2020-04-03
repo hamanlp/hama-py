44bb1ce9d44de26f51917b7043dac4a4dc1509ba
Seongmin Park <llov0708@gmail.com>
2020-04-03 00:31:49 -0400

    Add korean pos wrapper

--- src/hama/__init__.py
+++ src/hama/__init__.py
@@ -1,6 +1,7 @@
 from .tagging import tag
 from .pos import (nouns, predicates, symbols, modifiers, foreigns,
                   postpositions, suffixes, affixes, orthotones)
+from .korean_pos import (ches, yongs, soos, doks, jos, eoms, jubs)
 from .sequence import util
 from .sequence import similarity
 from .dict import Dict
---
+++ src/hama/korean_pos.py
@@ -0,0 +1,96 @@
+from pos import *
+
+
+def ches(text):
+    """Extract nouns (체언) from text.
+
+    Args:
+        text (str): Text to analyze.
+        
+    Returns:
+        list: List of morphemes tagged as nouns.
+
+    """
+
+    return nouns(text)
+
+
+def yongs(text):
+    """Extract predicates (용언) from text.
+
+    Args:
+        text (str): Text to analyze.
+        
+    Returns:
+        list: List of morphemes tagged as predicates.
+
+    """
+
+    return predicates(text)
+
+
+def soos(text):
+    """Extract modifiers (수식언) from text.
+
+    Args:
+        text (str): Text to analyze.
+        
+    Returns:
+        list: List of morphemes tagged as modifiers.
+
+    """
+    return modifiers(text)
+
+
+def doks(text):
+    """Extract orthotones (독립언) from text.
+
+    args:
+        text (str): text to analyze.
+        
+    returns:
+        list: list of morphemes tagged as orthotones.
+
+    """
+
+    return orthotones(text)
+
+
+def jos(text):
+    """Extract postpositions (관계언/조사) from text.
+
+    args:
+        text (str): text to analyze.
+        
+    returns:
+        list: list of morphemes tagged as postpositions.
+
+    """
+    return suffixes(text)
+
+
+def eoms(text):
+    """Extract suffixes (어미) from text.
+
+    args:
+        text (str): text to analyze.
+        
+    returns:
+        list: list of morphemes tagged as suffixes.
+
+    """
+    return
+
+
+def jubs(text):
+    """Extract affixes (접사) from text.
+
+    args:
+        text (str): text to analyze.
+        
+    returns:
+        list: list of morphemes tagged as affixes.
+
+    """
+
+    return affixes(text)
