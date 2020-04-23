dbe9e60044b0ebc3f26c3ba4aa585e792f8fba1e
Seongmin Park <llov0708@gmail.com>
2020-04-24 00:45:48 +0900

    Fix #11

---
+++ src/hama/init.py
@@ -0,0 +1,9 @@
+from hama.tagging.dict import Dict
+from hama.tagging.hmm import TagHMM
+
+def init(callback=None):
+    Dict().load()
+    TagHMM().load()
+
+    if callback is not None:
+        callback()
--- tests/hama/conftest.py
+++ tests/hama/conftest.py
@@ -1,8 +1,6 @@
 import pytest
-from hama.tagging.dict import Dict
-from hama.tagging.hmm import TagHMM
+import hama
 
 
 def pytest_runtest_setup(item):
-    Dict().load()
-    TagHMM().load()
+    hama.init()
