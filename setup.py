110948ae2a1706e1937760e906cfe952010dfee1
Seongmin Park <llov0708@gmail.com>
2020-04-25 00:37:38 +0900

    Add mmh3 dependency

--- setup.py
+++ setup.py
@@ -17,4 +17,5 @@ setup(name="hama",
       package_dir={'': 'src'},
       packages=find_packages(where='src'),
       include_package_data=True,
-      python_requires='>=3.6')
+      python_requires='>=3.6',
+      install_requires=['mmh3'])
